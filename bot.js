779ad10056d7d0719d91b87d6b2f6a6cc2034c65
c0range <43050456+c0range@users.noreply.github.com>
2020-03-22 08:14:29 +0900

    cough function

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ const prefix = config.prefix;
 //on startup
 client.on("ready", () => {
     console.log("login successful");
-})
+});
 
 //rest of code
 client.on("message", async (message) => {
@@ -16,16 +16,32 @@ client.on("message", async (message) => {
     //bot-specific commands
     if(command == prefix + "help") {
         //help command, maybe link to online document
-        message.channel.send();
+        message.channel.send("help command");
     }
 
     if(command == prefix + "about") {
-        message.channel.send("https://github.com/c0range/plaguebot");
+        message.channel.send("Hi I'm Plague Doctor (aka PlagueBot)! Learn more about me at https://github.com/c0range/plaguebot");
     }
 
     if(command == prefix + "start") {
         //start game
     }
-})
+
+    if(command == "cough" || command == "sneeze" || command =="spit") {
+        cough();
+
+        async function cough() {
+            const seconds = message.createdAt.getSeconds() + 60;
+            console.log(seconds);
+            message.channel.send(`${message.member.nickname} coughed at ${message.createdAt}`);
+            const mAll = await message.channel.messages.fetch({ limit: 10});
+            const mNoBot = mAll.filter(m => m.author.id !== '691029385846849566');
+            mNoBot.first(message => console.log(message.content));
+            
+            
+            
+        }
+    }
+});
 
 client.login(config.token);
