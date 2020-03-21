c81a1b8a9bff1aa3c4fcf4b84ab2c8c5237c25e8
c0range <43050456+c0range@users.noreply.github.com>
2020-03-22 07:01:49 +0900

    fix config directory

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //import discordjs and secret token
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require(".secret/config.json");
+const config = require("./secret/config.json");
 const prefix = config.prefix;
 
 //on startup
